2a3fecff349bea18477520378c0cc8275764065a
Akshay Chavan <akshaychavanaxy@gmail.com>
2022-09-22 12:16:11 +0530

    add record delete record

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,13 +5,16 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {  
+export class UserService {
 
   private empinfo = 'http://localhost:3000/api/v1/empinfo';
   private clientinfo = 'http://localhost:3000/api/v1/clientinfo';
 
-  recordsUrl = 'http://localhost:3000/api/v1/transactinfo';
   contactsUrl = 'http://localhost:3000/api/v1/emergencyinfo';
+  transactUrl = 'http://localhost:3000/api/v1/transactinfo';
+  empidTransactUrl = "http://localhost:3000/api/v1/transactinfo/empid";
+  clientUrl = "http://localhost:3000/api/v1/clientinfo";
+  taskUrl = "http://localhost:3000/api/v1/tasksinfo";
 
   constructor(private http: HttpClient) { }
 
@@ -19,15 +22,35 @@ export class UserService {
     return this.http.post<any>(this.empinfo, employee_data)
   }
 
+  getClients() {
+    return this.http.get(this.clientUrl);
+  }
+
+  getTasks() {
+    return this.http.get(this.taskUrl);
+  }
+
   addClient(client_data: any) {
     return this.http.post<any>(this.clientinfo, client_data)
   }
 
-  getRecords(){
-    return this.http.get(this.recordsUrl);
+  getRecords() {
+    return this.http.get(this.transactUrl);
   }
 
-  getContacts(){
+  getempidRecords() {
+    return this.http.get(this.empidTransactUrl);
+  }
+
+  deleteRecord(id: string) {
+    return this.http.delete(this.transactUrl + '/' + id);
+  }
+
+  getContacts() {
     return this.http.get(this.contactsUrl);
+  }
+
+  postTransact(addRecord: any) {
+    return this.http.post<any>(this.transactUrl, addRecord);
   }
 }
--- src/app/userdashboard/userdashboard.component.ts
+++ src/app/userdashboard/userdashboard.component.ts
@@ -2,9 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { UserService } from '../services/user.service';
+import { NgToastService } from 'ng-angular-popup';
 
 import * as html2pdf from 'html2pdf.js'
 import { ngxCsv } from 'ngx-csv/ngx-csv';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-userdashboard',
@@ -13,9 +15,13 @@ import { ngxCsv } from 'ngx-csv/ngx-csv';
 })
 export class UserdashboardComponent implements OnInit {
 
+  user: any;
   records: any;
   contacts: any;
-  user: any = {};
+  clients: any;
+  tasks: any;
+  todayDate = formatDate(new Date(), 'yyyy-MM-dd', 'en');
+
 
   empidItem = localStorage.getItem('empid');
   passwordItem = localStorage.getItem('password');
@@ -24,7 +30,8 @@ export class UserdashboardComponent implements OnInit {
     "password": this.passwordItem
   }
 
-  todayDate: Date = new Date();
+  addRecord: any = {}
+  empidrecords: any;
 
   show: boolean = false;
   showPassword() {
@@ -35,6 +42,7 @@ export class UserdashboardComponent implements OnInit {
     private router: Router,
     private authService: AuthService,
     private userService: UserService,
+    private toast: NgToastService,
   ) {
 
     this.authService.loginUser(this.loginData).subscribe((res) => {
@@ -47,10 +55,47 @@ export class UserdashboardComponent implements OnInit {
       this.records = res;
     })
 
+    this.userService.getempidRecords().subscribe((res) => {
+      console.log("Employee Specific Records", res);
+      this.empidrecords = res;
+    })
+
     this.userService.getContacts().subscribe((res) => {
       console.log("Emergency Contacts", res);
       this.contacts = res;
     })
+
+    this.userService.getClients().subscribe((res) => {
+      console.log("List of Clients", res);
+      this.clients = res;
+    })
+
+    this.userService.getTasks().subscribe((res) => {
+      console.log("List of Tasks", res);
+      this.tasks = res;
+    })
+  }
+
+  add_Record() {
+    console.log("Record added successfully");
+
+    this.userService.postTransact(this.addRecord).
+      subscribe(
+        res => {
+          console.log("Record added", res);
+          window.location.reload();
+        })
+    this.toast.success({ detail: "Success", summary: 'Record added successfully', duration: '3000' });
+  }
+
+  delete_Record(item: { trid: string; }) {
+    if (confirm('Are you sure you want to delete ?')) {
+      this.userService.deleteRecord(item.trid)
+        .subscribe(response => {
+          this.records = this.records.filter((item: { id: any; }) => item.id !== item.id);
+        });
+      window.location.reload();
+    }
   }
 
   download_pdf() {
