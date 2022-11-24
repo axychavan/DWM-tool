b43f3dd75c4e855e39f25aff5591616e6117981f
Akshay Chavan <akshaychavanaxy@gmail.com>
2022-11-24 20:59:28 +0530

    complete sortedRecords

--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -48,7 +48,7 @@ export class UserService {
   }
 
   customDate(data: any) {
-    return this.http.get<any>(this.customdate, data);
+    return this.http.post<any>(this.customdate, data);
   }
 
   addRecord(data: any) {
